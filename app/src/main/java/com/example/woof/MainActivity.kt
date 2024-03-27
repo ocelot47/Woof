b16a92a1aa2b2363b719285c038fb0357ab01ce4
LittleCooll <voquochoang2004@gmail.com>
2024-03-27 10:08:40 +0700

    27/3

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -35,6 +35,7 @@ import com.example.woof.ui.theme.WoofTheme
 
 //aaaaaaa
 
+//gggggg
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
